2a8e05bcbecc9612e5e4af9ae38c19f92c0569e7
Michael <theos.michael@gmail.com>
2019-11-13 01:04:42 +1100

    GraphToNetwork done (I think)

--- src/control.erl
+++ src/control.erl
@@ -1,62 +1,72 @@
 -module(control).
--export([graphToNetwork/1, extendNetwork/4]).
+-export([graphToNetwork/1, extendNetwork/4, sng/0]).
 
-graphToNetwork(Graph) =>
-    [].
+sng () ->
+  [{red  , [{white, [white, green]},
+	    {blue , [blue]}]},
+   {white, [{red, [blue]},
+	    {blue, [green, red]}]},
+   {blue , [{green, [white, green, red]}]},
+   {green, [{red, [red, blue, white]}]}
+  ].
+
+graphToNetwork(Graph) ->
+   Routers = ets:new(undef, [private]),
+   InboundCount = ets:new(undef, [private]),
+   % create and count procs
+   spawnRouters(Graph, InboundCount, Routers),
+   % At this point every router should exist and we can send init messages
+   initRouters(Graph, Routers, InboundCount),
+   io:format("Inbound ~p~n", [ets:match(InboundCount, '$1')]),
+   io:format("Routers ~p~n", [ets:match(Routers, '$1')]),
+   ets:delete(InboundCount),
+   ets:delete(Routers).
+
+initRouters(Graph, Routers, InboundCount) ->
+   lists:foreach(
+      fun({RouterName, Edges}) ->
+         io:format("Router ~p has edges ~p~n", [RouterName, Edges]),
+         Routes = mapRoutes(Edges, Routers, []),
+         [{_, Inbound}] = ets:lookup(InboundCount, RouterName),
+         [{_, Pid}] = ets:lookup(Routers, RouterName),
+         Pid ! {control, self(), self(), 0,
+            fun(_, Table) ->
+               ets:insert(Table, Routes),
+               ets:insert(Table, {'$NoInEdges', Inbound})
+            end}
+      end, Graph).
+
+spawnRouters(Graph, InboundCount, Routers) ->
+   lists:foreach(
+      fun({RouterName, Edges}) -> 
+         Pid = router:start(RouterName),
+         ets:insert(Routers, {RouterName, Pid}),
+         % Counting direct connections of routers will give the incoming edges
+         countDirect(Edges, InboundCount)
+      end, Graph).
+
+
+countDirect(Edges, InboundCount) ->
+   lists:foreach(
+      fun(Edge) ->
+         {Hop, _} = Edge,
+         Inbound = ets:lookup(InboundCount, Hop),
+         if Inbound == [] -> ets:insert(InboundCount, {Hop, 1});
+         true ->
+            [{_, N}] = Inbound, % list of tuples returned
+            ets:insert(InboundCount, {Hop, N + 1})
+         end
+      end, Edges).
+
+
+mapRoutes([], _, RouteMap) ->
+   RouteMap;
+mapRoutes([{Hop, Dests}|T], Routers, RouteMap) ->
+   % Should this be guarded?
+   [{_,Pid}] = ets:lookup(Routers, Hop),
+   Routes = lists:map(fun(Dest) -> {Dest, Pid} end, Dests),
+   mapRoutes(T, Routers, RouteMap ++ Routes).
+
 
 extendNetwork(RootPid, SeqNum, From, EdgeMap) ->
-    [].
-
-%% Circular network consisting of three nodes.
-%%
-circularNetwork3 () ->
-  [{red  , [{white, [white, blue]}]},
-   {white, [{blue , [red, blue]}]},
-   {blue , [{red  , [red, white]}]}
-  ].
-
-%% Reverse circle as in circularNetwork3/0
-%%
-reverseCircularNetwork3 () ->
-  [{red  , [{blue , [white, blue]}]},
-   {white, [{red  , [red, blue]}]},
-   {blue , [{white, [red, white]}]}
-  ].
-
-%% Build a circular network and then use a control request to change the 
-%% direction of all edges; verify the original and reversed network
-%%
-runTest () ->
-  io:format ("*** Starting router network...~n"),
-  CGraph = circularNetwork3 (),
-  RedPid = control:graphToNetwork (CGraph),
-  networkTest:verifyNetwork (RedPid, CGraph),
-
-  {WhitePid, _} = networkTest:probeNetwork (RedPid, white),
-  {BluePid , _} = networkTest:probeNetwork (RedPid, blue ),
-  if (WhitePid == undef) or (BluePid == undef) -> 
-      io:format ("*** ERROR: Corrupt network!~n");
-     true -> true
-  end,
-
-  io:format ("*** Reversing routing cycle...~n"),
-  RedPid ! {control, self (), self (), 1, 
-	    fun (Name, Table) -> 
-		case Name of
-		  red   -> ets:insert (Table, [{white, BluePid }, 
-					       {blue , BluePid }]);
-		  white -> ets:insert (Table, [{red  , RedPid  }, 
-					       {blue , RedPid  }]);
-		  blue  -> ets:insert (Table, [{red  , WhitePid}, 
-					       {white, WhitePid}])
-		end,
-		[]
-	    end},
-  receive
-    {committed, RedPid, 1} -> io:format ("*** ...done.~n");
-    {abort    , RedPid, 1} -> 
-      io:format ("*** ERROR: Re-configuration failed!~n")
-  after 10000              ->
-      io:format ("*** ERROR: Re-configuration timed out!~n")
-  end,
-  networkTest:verifyNetwork (RedPid, reverseCircularNetwork3 ()).
+   wip.
--- src/router.erl
+++ src/router.erl
@@ -2,56 +2,72 @@
 -export([start/1]).
 
 start (RouterName) ->
-    io:format ("Starting router: ~p~n", [RouterName]),
-    spawn(fun() -> init(RouterName) end).
+   spawn(fun() -> init(RouterName) end).
+
 
 init(RouterName) ->
-    RoutingTable = ets:new(table, [private]),
-    ets:insert(RoutingTable, {'$NoInEdges', 0}),
-    loop(RouterName, RoutingTable).
+   io:format("Starting router: ~p with pid ~p~n", [RouterName, self()]),
+   RoutingTable = ets:new(table, [private]),
+   ets:insert(RoutingTable, {'$NoInEdges', 0}),
+   loop(RouterName, RoutingTable).
+
 
 loop (RouterName, RoutingTable) ->
-    receive
-        {message, Dest, From, Pid, Trace} ->
-            handleMessage(RouterName, RoutingTable, Dest, From, Pid, Trace),
-            loop(RouterName, RoutingTable);
-        {control, From, Pid, SeqNum, ControlFun} ->
-            handleControl(RouterName, RoutingTable, From, Pid, SeqNum, ControlFun),
-            loop(RouterName, RoutingTable);
-        {dump, From} ->
-            handleDump(RoutingTable, From),
-            loop(RouterName, RoutingTable);
-        stop ->
-            handleStop(RouterName, RoutingTable)
-    end.
+   receive
+      {message, Dest, From, Pid, Trace} ->
+         handleMessage(RouterName, RoutingTable, Dest, From, Pid, Trace),
+         loop(RouterName, RoutingTable);
+      {control, From, Pid, SeqNum, ControlFun} ->
+         handleControl(RouterName, RoutingTable, From, Pid, SeqNum, ControlFun),
+         loop(RouterName, RoutingTable);
+      {dump, From} ->
+         handleDump(RoutingTable, From),
+         loop(RouterName, RoutingTable);
+      stop ->
+         handleStop(RouterName, RoutingTable)
+   end.
 
-handleMessage (RouterName, RoutingTable, Dest, From, Pid, Trace) ->
-    io:format("Received a message"),
-    NewTrace = [RouterName|Trace],
-    if
-        Dest == RouterName ->
-            Pid ! {trace, self(), lists:reverse(NewTrace)};
-        true ->
-            %% Check if Next is empty/wrong
-            Next = ets:lookup(RoutingTable, Dest),
-            Next ! {message, Dest, self(), Pid, NewTrace}
-    end.
+
+handleMessage (RouterName, RoutingTable, Dest, _, Pid, Trace) ->
+   io:format("Received a message~n"),
+   NewTrace = [RouterName|Trace],
+   if
+      Dest == RouterName ->
+         Pid ! {trace, self(), lists:reverse(NewTrace)};
+      true ->
+         %% Check if Next is empty/wrong
+         Next = ets:lookup(RoutingTable, Dest),
+         if
+            Next /= [] ->
+               Next ! {message, Dest, self(), Pid, NewTrace};
+            true ->
+               %% Bad things have happened...
+               io:format("Router ~p has received a message to ~p it can't route~n", [RouterName, Dest])
+         end
+   end.
+
 
 handleControl (RouterName, RoutingTable, From, Pid, SeqNum, ControlFun) ->
-        if
-            SeqNum == 0 ->
-                %% init
-                Children = ControlFun(RouterName, RoutingTable),
-                body;
-            true ->
-                %% not init
-                body
-        end.
+   if SeqNum == 0 -> %% init
+      Children = ControlFun(RouterName, RoutingTable),
+      io:format("Children: ~p~n", [Children]),
+      if Children == abort ->
+            body;
+         true ->
+            body
+      end;
+   true ->
+      %% not init
+      body
+   end.
+
 
 handleDump (RoutingTable, From) ->
-    io:format ("Taking a dump"),
-    Dump = ets:match(RoutingTable, '$1'),
-    From ! {table, self(), Dump}.
+   io:format ("Taking a dump~n"),
+   Dump = ets:match(RoutingTable, '$1'),
+   From ! {table, self(), Dump}.
+
 
 handleStop (RouterName, RoutingTable) ->
-    io:format ("Stopping router: ~p~n", [RouterName]).
+   ets:delete(RoutingTable),
+   io:format ("Stopping router: ~p~n", [RouterName]).
