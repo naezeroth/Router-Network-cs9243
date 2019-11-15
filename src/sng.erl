702b795abc2951c302783d6aab4bdc7ed4a7f2ad
Michael Theos <mtheos@nettex.com.au>
2019-11-15 14:48:38 +1100

    Test graph

---
+++ src/sng.erl
@@ -0,0 +1,11 @@
+-module(sng).
+-export([sng/0]).
+
+sng () ->
+  [{red, [{white, [white, green]},
+	    {blue, [blue]}]},
+   {white, [{red, [blue]},
+	    {blue, [green, red]}]},
+   {blue, [{green, [white, green, red]}]},
+   {green, [{red, [red, blue, white]}]}
+  ].
